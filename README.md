9803017916f4b6ee1029d8902bcf6da0ca2a598b
methk <matteovhberti@gmail.com>
2022-05-08 11:44:10 +0200

    Add a simple description in the README and reformat the code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# digital-restaurant-app
+# Digital Restaurant
+A simple Spring Boot + React application to manage orders in a restaurant.
--- src/main/java/com/digital/DigitalRestaurantApp.java
+++ src/main/java/com/digital/DigitalRestaurantApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DigitalRestaurantApp {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DigitalRestaurantApp.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DigitalRestaurantApp.class, args);
+  }
 
 }
